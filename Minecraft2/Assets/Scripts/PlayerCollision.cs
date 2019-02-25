e75419ffc25c967982423dc8a97757a45afffa2a
SharkBearGator-PC <47893402+SharkBearGator@users.noreply.github.com>
2019-02-25 09:55:41 -0600

    post processing

--- Minecraft2/Assets/Scripts/PlayerCollision.cs
+++ Minecraft2/Assets/Scripts/PlayerCollision.cs
@@ -12,6 +12,7 @@ public class PlayerCollision : MonoBehaviour
         if(collision.gameObject.tag == "Obstacle")
         {
             rb.AddForce(0, 800, -800);
+            rb.AddTorque(0, 300, 300);
             Invoke("DoIt", 0.2f);
             FindObjectOfType<GameManager>().EndGame();
         }
