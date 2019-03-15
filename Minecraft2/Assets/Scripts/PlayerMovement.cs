51c940e01ac75a4853f8f5a6bd2d80068b455194
SharkBearGator-PC <47893402+SharkBearGator@users.noreply.github.com>
2019-03-15 00:18:37 -0500

    more proper input

--- Minecraft2/Assets/Scripts/PlayerMovement.cs
+++ Minecraft2/Assets/Scripts/PlayerMovement.cs
@@ -5,10 +5,10 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public Rigidbody rb;
+
     public float PlayerControl = 500f;
     public float PlayerSpeed;
-    bool left = false;
-    bool right = false;
+    float horizontal;
 
     private void Update()
     {
@@ -16,35 +16,15 @@ public class PlayerMovement : MonoBehaviour
             FindObjectOfType<GameManager>().EndGame();
             FindObjectOfType<PlayerCollision>().DoIt();
         }
-        if (Input.GetKey("a"))
-        {
-            left = true;
-        }
-        else if (Input.GetKey("d"))
-        {
-            right = true;
-        }
-        else
-        {
-            left = false;
-            right = false;
-        }
-        
+
+
     }
     // Update is called once per frame
     void FixedUpdate()
     {
         rb.AddForce(0,0,PlayerSpeed * Time.deltaTime);
 
-        if (left == true)
-        {
-            rb.AddForce(-PlayerControl * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
-            Debug.Log("going left");
-        }
-        if (right == true)
-        {
-            rb.AddForce(PlayerControl * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
-            Debug.Log("going right");
-        }
+        horizontal = Input.GetAxis("Horizontal") * PlayerControl * Time.deltaTime;
+        rb.transform.Translate(horizontal, 0, 0);
     }
 }
