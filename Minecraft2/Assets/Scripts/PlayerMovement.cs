520a6562c2de107689ca7a661893a6a9282d8a65
SharkBearGator-PC <47893402+SharkBearGator@users.noreply.github.com>
2019-03-17 16:14:56 -0500

    flipping gravity works, try to adjust camera now

--- Minecraft2/Assets/Scripts/PlayerMovement.cs
+++ Minecraft2/Assets/Scripts/PlayerMovement.cs
@@ -8,22 +8,36 @@ public class PlayerMovement : MonoBehaviour
 
     public float PlayerControl = 500f;
     public float PlayerSpeed;
+    public float FallSpeed;
     float horizontal;
+    public bool gravity=false;
 
     private void Update()
     {
-        if (rb.position.y < 0){
+        if (rb.position.y < 0)
+        {
             FindObjectOfType<GameManager>().EndGame();
             FindObjectOfType<PlayerCollision>().DoIt();
         }
 
+        if (Input.GetButtonDown("Jump"))
+        {
+            gravity = !gravity;
+        }
 
+        if (gravity == true)
+        {
+            rb.AddForce(0, FallSpeed, 0);
+        }
+        if (gravity == false)
+        {
+            rb.AddForce(0, -FallSpeed, 0);
+        }
     }
     // Update is called once per frame
     void FixedUpdate()
     {
         rb.AddForce(0,0,PlayerSpeed * Time.deltaTime);
-
         horizontal = Input.GetAxis("Horizontal") * PlayerControl * Time.deltaTime;
         rb.transform.Translate(horizontal, 0, 0);
     }
