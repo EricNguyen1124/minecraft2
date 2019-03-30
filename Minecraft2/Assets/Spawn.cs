cf5bec4623c0318ebd9e722a47bcae6a362ba1e4
SharkBearGator-PC <47893402+SharkBearGator@users.noreply.github.com>
2019-03-29 19:33:57 -0500

    um please save spawning

---
+++ Minecraft2/Assets/Spawn.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawn : MonoBehaviour
+{
+    public GameObject prefab;
+    Vector3 pos = new Vector3(0, 0.5f, 50);
+    int rng;
+    bool over = false;
+    
+
+
+    private void Update()
+    {
+        while(over == false)
+        {
+            Debug.Log("spawn");
+            Instantiate(prefab, new Vector3(0, 0.5f, 80), Quaternion.identity);
+        }
+    }
+}
